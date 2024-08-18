df3dd94b7de7edf72fe518993def11f0d11d4577
chi@chi.com <chi@chi.com>
2024-08-18 20:42:04 +0100

    i

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
