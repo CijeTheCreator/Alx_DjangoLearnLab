5629163100d7c840ea5833393f66d517f10c0813
chi@chi.com <chi@chi.com>
2024-08-18 20:50:18 +0100

    i

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, register
+from . import views
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
