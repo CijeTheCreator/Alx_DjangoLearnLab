c5955443f2ae739c60775f1939e6b425e2d19f7b
chi@chi.com <chi@chi.com>
2024-08-24 21:03:49 +0100

    commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -8,4 +9,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
 ]
