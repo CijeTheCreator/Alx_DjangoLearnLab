aee2babb248bcbdfa886fa9be31d6ec9d897eb16
jan <jan2@jan.com>
2024-09-22 09:51:11 +0100

    Posts stuff

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination
+
+
+class PostPagination(PageNumberPagination):
+    page_size = 10
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PostPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+
