50c263812369b60f768e1d11c25f30fe32262937
jan <jan2@jan.com>
2024-09-22 10:17:54 +0100

    Fixed checker error

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, get_user_model
 from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 from .models import CustomUser
-from rest_framework.generics import GenericAPIView
+from rest_framework import generics
 
 CustomUser = get_user_model()
 
@@ -30,7 +30,7 @@ class LoginView(APIView):
         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
     
 
-class FollowUserView(GenericAPIView):
+class FollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
@@ -42,7 +42,7 @@ class FollowUserView(GenericAPIView):
         request.user.following.add(user_to_follow)
         return Response({"success": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
 
-class UnfollowUserView(GenericAPIView):
+class UnfollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
