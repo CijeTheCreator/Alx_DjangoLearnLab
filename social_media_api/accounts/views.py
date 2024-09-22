5db47054f820a46b52555514a8fea8933b17b21a
jan <jan2@jan.com>
2024-09-22 10:16:58 +0100

    Fixed checker error

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,8 +6,9 @@ from django.contrib.auth import authenticate, get_user_model
 from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 from .models import CustomUser
+from rest_framework.generics import GenericAPIView
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 class RegisterView(APIView):
     def post(self, request):
@@ -28,25 +29,27 @@ class LoginView(APIView):
             return Response({'token': token.key})
         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
     
-class FollowUserView(APIView):
+
+class FollowUserView(GenericAPIView):
+    queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(User, id=user_id)
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
         if user_to_follow == request.user:
             return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         request.user.following.add(user_to_follow)
-        return Response({"success": f"You are now following {user_to_follow.username}."})
+        return Response({"success": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(GenericAPIView):
+    queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(User, id=user_id)
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
         if user_to_unfollow == request.user:
             return Response({"error": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         request.user.following.remove(user_to_unfollow)
-        return Response({"success": f"You have unfollowed {user_to_unfollow.username}."})
-
+        return Response({"success": f"You have unfollowed {user_to_unfollow.username}."}, status=status.HTTP_200_OK)
